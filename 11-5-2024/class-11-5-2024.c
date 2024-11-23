513a6e720e585abb1dfa1d126f6108dd4a98e0d4
fopce403 <fopce403sharif@gmail.com>
2024-11-23 22:39:13 +0330

    Add files via upload

---
+++ 11-5-2024/class-11-5-2024.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+
+
+// int main(){
+//     int day ;
+//     scanf("%d" , &day);
+//     switch(day){
+//         case 0 :
+//             printf("shanbe\n") ;
+//             break ;
+//         case 1 :
+//             printf("Yek\n") ;
+//             break ;
+//         case 2 : 
+//             printf("Do\n") ;
+//             break ;
+//         default :
+//             printf("BO WHO!!\n") ;
+//             break ;
+//     }
+//     return 0 ;
+// }
+
+
+
+// int main(){
+//     int a , b ;
+//     scanf( "%d" , &a ) ;
+//     switch( a % 10 ){
+//         case 1 :
+//             printf("%dst\n" , a) ;
+//             break ; // watch it as a label witch runs the code block.
+//         case 2 :
+//             printf("%dnd\n" , a) ; 
+//             break ;
+//         case 3 :
+//             printf("%drd\n" , a) ; 
+//             break ;
+//         default :
+//             printf("%dth\n" , a) ;
+//             break ;
+//     }
+//     return 0 ;
+// }
+
+
+
+// for loop
+// int main(){
+//     for(variable ; condition ; to do?){
+//         job;
+//     }
+//     return 0 ;
+// }
+
+
+
+// int main(){
+//     int n ; //getting n numbers 
+//     int sum = 0 ; // getting sum value
+//     scanf("%d" , &n) ; \\ for loop cause of summations
+//     for( int i = n ; i > 0 ; i-- ){
+//         int a;
+//         scanf("%d" , &a) ;
+//         sum += a ;
+//     }
+//     printf("%d\n" , sum);
+//     return 0 ;
+// }
+
+
+
+// writing binary form of a numbers
+// int main(){
+//     int a ;
+//     scanf("%d" , &a) ;
+
+
+
+//     for(int i = 31 ; i >= 0 ; i-- ){
+//         int shifter = 1 << i ; 
+//         if((shifter & a) > 0){
+//             printf("1");
+//         }
+//         else{
+//             printf("0");
+//         }
+//     }
+//     return 0 ;
+// }
+
+
+
+// int main(){
+//     for(int i = 0 ; i < 10 || printf("\n") > 5 ; i++ ){ 
+//         printf("%d" , i) ;
+//     }
+//     return 0 ;
+// }
+// until  i < 10 writes 0 to 9 after i == 10 it goes in second condition
+
+// int main(){
+//     int a = printf("ss\n");
+//     for(int i = 0 ; i < 10 || a > 5 ; i++ ){ 
+//         printf("%d" , i) ;
+//     }
+//     return 0 ;
+// }
+
+
+
+//ternary operations :
+// int main(){
+//     int a = 5 ;
+//     a % 2 == 0 ? printf("Even") : printf("Odd") ;
+//     return 0 ;
+// }
+
+
